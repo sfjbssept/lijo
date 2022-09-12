397c6368d079e0902abde97700b7cd8a76ddd708
lijo Joseph <lijo.8893@gmal.com>
2022-09-12 04:20:35 +0000

    commit

--- ProductsManagementSystem/src/main/java/com/product/ProductsManagementSystemApplication.java
+++ ProductsManagementSystem/src/main/java/com/product/ProductsManagementSystemApplication.java
@@ -1,9 +1,11 @@
 package com.product;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAutoConfiguration
 public class ProductsManagementSystemApplication {
 
 	public static void main(String[] args) {
