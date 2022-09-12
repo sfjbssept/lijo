bfae096f437549f34cd90a4801c2d7ecf639f226
lijo Joseph <lijo.8893@gmal.com>
2022-09-12 03:43:45 +0000

    TRG-2022- sep 12 intial commit with spring doc open api

---
+++ ProductsManagementSystem/src/main/java/com/product/controller/ProductController.java
@@ -0,0 +1,18 @@
+package com.product.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(path = "api/v1")
+public class ProductController {
+	
+	
+	@GetMapping(value = "test")
+	public String get() {
+		return null;
+		
+	}
+
+}
